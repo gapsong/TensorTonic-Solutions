0fcf07bee52507a45a26e0c599f0bd713c4efb37
gapsong <gapsong@users.noreply.github.com>
2026-03-07 13:36:28 +0530

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -2,20 +2,11 @@ import numpy as np
 
 def positional_encoding(seq_len, d_model, base=10000.0):
     result = np.zeros((seq_len, d_model))
-    
-    pos = np.arange(seq_len)[:, None]
 
-    div_term = np.power(base, np.arange(0, d_model, 2) / d_model)
+    positions = np.arange(0, seq_len)[:, None]
 
+    div = np.power(base, np.arange(0, d_model, 2) / d_model)
 
-    result[:, 0::2] = np.sin(pos / div_term)
-    
-    if d_model % 2 == 0:
-    
-
-    
-        result[:, 1::2] = np.cos(pos / div_term)
-    else:
-        result[:, 1::2] = np.cos(pos / div_term[:-1])
-        
+    result[:, ::2] = np.sin(positions / div)
+    result[:, 1::2] = np.cos(positions / div)[:, :d_model // 2]
     return result
